1849446cffe8f73f850e8b0710a9d831d7f132b2
Sercan <sercansimsek44@gmail.com>
2026-07-27 12:46:56 +0200

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require("mongodb-legacy");
 const assert = require("assert");
+const dboper = require("./operations");
 
 const url = "mongodb://127.0.0.1:27017/";
 const dbName = "nucampsite";
@@ -13,30 +14,46 @@ MongoClient.connect(url, {}, (err, client) => {
 
   db.dropCollection("campsites", (err, result) => {
     assert.strictEqual(err, undefined);
-
-    console.log("Dropped Collection", result);
-
-    const collection = db.collection("campsites");
+    console.log("Dropped Collection:", result);
 
     const documentToInsert = {
       name: "Breadcrumb Trail Campground",
       description: "Test",
     };
 
-    collection.insertOne(documentToInsert, (err, result) => {
-      assert.strictEqual(err, undefined);
-
+    dboper.insertDocument(db, documentToInsert, "campsites", (result) => {
       console.log("Insert Document:", {
         _id: result.insertedId,
         ...documentToInsert,
       });
 
-      collection.find().toArray((err, docs) => {
-        assert.strictEqual(err, undefined);
-
+      dboper.findDocuments(db, "campsites", (docs) => {
         console.log("Found Documents:", docs);
 
-        client.close();
+        dboper.updateDocument(
+          db,
+          { name: "Breadcrumb Trail Campground" },
+          { description: "Updated Test Description" },
+          "campsites",
+          (result) => {
+            console.log("Updated Document Count:", result.modifiedCount);
+
+            dboper.findDocuments(db, "campsites", (docs) => {
+              console.log("Found Documents:", docs);
+
+              dboper.removeDocument(
+                db,
+                { name: "Breadcrumb Trail Campground" },
+                "campsites",
+                (result) => {
+                  console.log("Deleted Document Count:", result.deletedCount);
+
+                  client.close();
+                },
+              );
+            });
+          },
+        );
       });
     });
   });
